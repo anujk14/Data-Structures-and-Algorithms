a481211d1b3b0754e3e05e06eac1227bf69614a9
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2019-01-27 17:27:05 +0530

    Adds array rotation problem + solution

---
+++ Array/array_rotation.py
@@ -0,0 +1,33 @@
+'''
+Q: Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+URL: https://www.geeksforgeeks.org/array-rotation/
+'''
+
+def rotate(arr, rot):
+    rotated_array = []
+    rot = rot % len(arr)
+
+    for i in range(0, len(arr)):
+        rotated_array.append(arr[(i+rot)%len(arr)])
+
+    return rotated_array
+
+
+def main():
+    n = int(input("Enter size of array\n"))
+    d = int(input("Enter the number of rotations to be performed\n"))
+    arr = []
+    print("Enter {size} elements".format(size=n))
+    for i in range(0, n):
+        a = int(input())
+        arr.append(a)
+
+    result = rotate(arr, d)
+
+    print(result)
+
+main()
+
+
+1, 2, 3, 4, 5
+d = 2
